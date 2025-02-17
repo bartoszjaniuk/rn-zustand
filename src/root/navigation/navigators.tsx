36cf70b9403686a9d37b08be7468f30e8c473dd4
Bartosz <janiukbartosz@gmail.com>
2025-02-17 07:48:30 +0100

    work in progress, routing

--- src/root/navigation/navigators.tsx
+++ src/root/navigation/navigators.tsx
@@ -3,7 +3,33 @@ import { DetailsScreen } from "../screens/details/Details";
 import { HomeScreen } from "../screens/home/Home";
 import { ProfileScreen } from "../screens/profile/Profile";
 
-const NativeStack = createNativeStackNavigator();
+// type DynamicParamList = {
+// 	// HomeTabs: undefined;
+// 	// Profile: { user: string };
+// 	// Settings: undefined;
+// 	// NotFound: undefined;
+// 	Home: { postId: string };
+// 	Details: { user: string };
+// 	CreatePost: undefined;
+// 	UlumuluStyled: undefined;
+// 	WithHeaderCustomComponent: undefined;
+// 	HeaderWithButton: undefined;
+// };
+
+// // Use the dynamic parameter list in the global namespace declaration
+// declare global {
+// 	namespace ReactNavigation {
+// 		interface RootParamList extends DynamicParamList {}
+// 	}
+// }
+
+export type RootParamList = {
+  Home: undefined;
+  Details: { user: string };
+  Profile: undefined;
+};
+
+const NativeStack = createNativeStackNavigator<RootParamList>();
 
 export const RootNavigator = () => {
   return (
--- src/root/screens/home/Home.tsx
+++ src/root/screens/home/Home.tsx
@@ -3,9 +3,11 @@ import { View, Button, Text } from "react-native";
 import { useAuthStore } from "../../../store/authStore";
 import { useUserInfoQuery } from "../../../api/user/hooks/useUserInfoQuery";
 import { AnimatedSplashScreen } from "../../../shared/SplashScreen";
+import { useNavigation } from "@react-navigation/native";
 
 export const HomeScreen = () => {
   const auth = useAuthStore();
+  const navigate = useNavigation();
 
   const userInfoQuery = useUserInfoQuery();
 
@@ -14,6 +16,10 @@ export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen - {userInfoQuery.data?.email}</Text>
+      <Button
+        title="Go to details"
+        onPress={() => navigate.navigate("Details", { user: "John Doe" })}
+      />
       <Button title="Wyloguj" onPress={auth.logout} />
     </View>
   );
